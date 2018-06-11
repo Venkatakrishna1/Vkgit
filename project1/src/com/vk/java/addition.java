e3b3fbbfb4b61041c1fd1424c75d43c06045fbc1
Lenovo <Lenovo@Lenovo-PC>
2018-06-11 10:38:24 +0530

    Devp2------> print Git

--- project1/src/com/vk/java/addition.java
+++ project1/src/com/vk/java/addition.java
@@ -5,7 +5,8 @@ public class addition {
 	public static void main(String[] args) {
 		int a=5,b=9,c;
 		c=a+b;
-		System.out.println(c);
+		//System.out.println(c);
+		System.out.println("hello Git");
 
 	}
 
